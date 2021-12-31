df976fb5d875a3606a76c800082f3dd3d3a59878
tdr <tdr@ucsb.edu>
2021-12-31 11:53:00 -0800

    upload lab 0

---
+++ labs/lab0-gettingstarted/tests/q2_a.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2_a',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> type(arr) == np.ndarray\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> arr[4] == 5\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ labs/lab0-gettingstarted/tests/q2_c.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q2_c',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> type(array_sum(a,b)) == np.ndarray\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> (array_sum([1], [1]) == np.array([2])).all()\nTrue', 'hidden': False, 'locked': False},
+                                   {   'code': '>>> array1 = [1,2,3,4,5];\n>>> array2 = [2,4,6,8,10];\n>>> (array_sum(array1, array2) == np.array([9,68,225,528,1025])).all()\nTrue',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
